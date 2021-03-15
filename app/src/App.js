fdaa8e0ae6228acc13da4aebc5d81edcdab72b16
ekohilas <ekohilas@users.noreply.github.com>
2021-03-15 22:04:34 +1100

    updated html to work with original game

--- app/src/App.js
+++ app/src/App.js
@@ -13,9 +13,25 @@ function App() {
 
         <button onClick={() => main.start_game(words)}> Start </button>
 
-        {/* below is just dummy stuff so js can find the id of the button */}
-        <button id="ready"></button>
-        <button id="timer"></button>
+        {/* below is just dummy testing stuff*/}
+        <div id='state'>
+            <div id='timer'></div>
+            <div id='board'>
+                <div id='category'></div>
+                <div id='position'></div>
+            </div>
+            <pre id='teams'></pre>
+        </div>
+        <div id='controls'>
+            <button id='start'>Start</button>
+            <button id='defer'>Defer</button>
+            <button id='win'>Win</button>
+            <button id='discard'>Discard</button>
+            <button id='ready'>Ready</button>
+            <button id='end'>End</button>
+        </div>
+        <pre id='words'>
+        </pre>
 
       </header>
     </div>
