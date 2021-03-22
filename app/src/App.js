5c2057ee8e46a87c2797a221ba6f079072ea76dd
vfang <makeitfang@gmail.com>
2021-03-22 23:55:12 +1100

    added timer

--- app/src/App.js
+++ app/src/App.js
@@ -2,20 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 import * as main from './api/main';
 import * as api from './api/api';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import words from './static/words.json'
 
 function App() {
   const [game, setGame] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(-1);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    if (timeLeft > 0) {
+      setTimeout(() => {
+        setTimeLeft(timeLeft - 1)
+      }, 1000)
+    }
+    else if (timeLeft == 0 && playStatus == true) {
+      setPlayStatus(false);
+      game.end_turn();
+    }
+  }, [timeLeft]);
+
+  const startRound = () => {
+    setPlayStatus(true);
+    setTimeLeft(10);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <button onClick={() => {
-          setGame(main.start_game(words));
-        }}> Start </button>
+        {game === null && 
+          <button onClick={() => {
+            setGame(main.start_game(words));
+          }}> Create Game </button>
+        }
         <button onClick={() => console.log(game)}>Print Game State</button>
         
         {/* below is just dummy testing stuff*/}
@@ -27,6 +48,14 @@ function App() {
             </div>
             <pre id='teams'></pre>
         </div>
+
+        {playStatus == false ? 
+          <div>
+            <button onClick={() => startRound()}>Start Round</button>
+          </div>
+        : <h1>Time Remaining: {timeLeft}</h1>
+        }
+
 
         {game !== null && 
           <div id='controls'>
