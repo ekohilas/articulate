4bb31520f82fb2f416d7347a34b4f0ef991a0fe0
vfang <makeitfang@gmail.com>
2021-05-26 21:59:10 +1000

    fix start turn bug

--- app/src/components/GameBoard.js
+++ app/src/components/GameBoard.js
@@ -102,7 +102,7 @@ export default function GameBoard(props) {
 
     const onReadyTurn = (direction) => {
         if (direction === 'right') {
-            setTurnStatus(true);
+            startTurn();
         }
     }
 
